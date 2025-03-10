cb04173d42019099d7fb83ca34ecc46a75a470b5
alqof <alqofnuraji@gmail.com>
2025-03-11 06:59:35 +0700

    update

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher([
-    '/sign-in(.*)',
-    '/sign-up(.*)'
-])
+// const isPublicRoute = createRouteMatcher([
+//     '/sign-in(.*)',
+//     '/sign-up(.*)'
+// ])
+const isProtectedRoute = createRouteMatcher(['/', '/api/webhooks/clerk', '/api/webhooks/stripe'])
   
 export default clerkMiddleware(async (auth, req) => {
-    if (!isPublicRoute(req)) {
-        await auth.protect()
-    }
+    // if (!isPublicRoute(req)) {
+    //     await auth.protect()
+    // }
+    if (isProtectedRoute(req)) await auth.protect()
 })
 
 export const config = {
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,86 +1,82 @@
 /* eslint-disable no-unused-vars */
-
-// ====== USER PARAMS
+// ====== USER PARAMS ======
 declare type CreateUserParams = {
     clerkId: string;
     email: string;
     username: string;
-    firstName: string;
-    lastName: string;
+    firstName: string | null;
+    lastName: string | null;
     photo: string;
 };
-  
 declare type UpdateUserParams = {
-    firstName: string;
-    lastName: string;
+    firstName: string | null;
+    lastName: string | null;
     username: string;
     photo: string;
 };
   
 
-// ====== IMAGE PARAMS
+// ====== IMAGE PARAMS ======
 declare type AddImageParams = {
     image: {
-    title: string;
-    publicId: string;
-    transformationType: string;
-    width: number;
-    height: number;
-    config: any;
-    secureURL: string;
-    transformationURL: string;
-    aspectRatio: string | undefined;
-    prompt: string | undefined;
-    color: string | undefined;
+        title: string;
+        publicId: string;
+        transformationType: string;
+        width: number;
+        height: number;
+        config: any;
+        secureURL: string;
+        transformationURL: string;
+        aspectRatio: string | undefined;
+        prompt: string | undefined;
+        color: string | undefined;
     };
     userId: string;
     path: string;
 };
-
 declare type UpdateImageParams = {
     image: {
-    _id: string;
-    title: string;
-    publicId: string;
-    transformationType: string;
-    width: number;
-    height: number;
-    config: any;
-    secureURL: string;
-    transformationURL: string;
-    aspectRatio: string | undefined;
-    prompt: string | undefined;
-    color: string | undefined;
+        _id: string;
+        title: string;
+        publicId: string;
+        transformationType: string;
+        width: number;
+        height: number;
+        config: any;
+        secureURL: string;
+        transformationURL: string;
+        aspectRatio: string | undefined;
+        prompt: string | undefined;
+        color: string | undefined;
     };
     userId: string;
     path: string;
 };
-
 declare type Transformations = {
     restore?: boolean;
     fillBackground?: boolean;
     remove?: {
-    prompt: string;
-    removeShadow?: boolean;
-    multiple?: boolean;
+        prompt: string;
+        removeShadow?: boolean;
+        multiple?: boolean;
     };
     recolor?: {
-    prompt?: string;
-    to: string;
-    multiple?: boolean;
+        prompt?: string;
+        to: string;
+        multiple?: boolean;
     };
     removeBackground?: boolean;
 };
 
 
-// ====== TRANSACTION PARAMS
+
+// ====== TRANSACTION PARAMS ======
 declare type CheckoutTransactionParams = {
     plan: string;
     credits: number;
     amount: number;
     buyerId: string;
 };
-
 declare type CreateTransactionParams = {
     stripeId: string;
     amount: number;
@@ -89,32 +85,28 @@ declare type CreateTransactionParams = {
     buyerId: string;
     createdAt: Date;
 };
-
 declare type TransformationTypeKey = | "restore" | "fill" | "remove" | "recolor" | "removeBackground";
 
-// ====== URL QUERY PARAMS
+
+// ====== URL QUERY PARAMS ======
 declare type FormUrlQueryParams = {
     searchParams: string;
     key: string;
     value: string | number | null;
 };
-
 declare type UrlQueryParams = {
     params: string;
     key: string;
     value: string | null;
 };
-
 declare type RemoveUrlQueryParams = {
     searchParams: string;
     keysToRemove: string[];
 };
-
 declare type SearchParamProps = {
     params: { id: string; type: TransformationTypeKey };
     searchParams: { [key: string]: string | string[] | undefined };
 };
-
 declare type TransformationFormProps = {
     action: "Add" | "Update";
     userId: string;
@@ -123,7 +115,6 @@ declare type TransformationFormProps = {
     data?: IImage | null;
     config?: Transformations | null;
 };
-
 declare type TransformedImageProps = {
     image: any;
     type: string;
